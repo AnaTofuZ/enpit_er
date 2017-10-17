c7ff7978adc845551758a5586aa32f2a3cecc1cd
AnaTofuZ <anatofuz@gmail.com>
2017-10-17 13:31:18 +0900

    rootingのpathを修正

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,12 +2,12 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get help_path
     assert_response :success
   end
 
