fc7944ec18d0d04eb62c89ca3a8cd69d85689850
AnaTofuZ <anatofuz@gmail.com>
2017-10-18 15:25:30 +0900

    deviseでUserモデルを作成(migrateしていない)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
-    validates :name,  presence: true, length: { maximum: 50 }
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and  
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+  validates :name,  presence: true, length: { maximum: 50 }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, length: { maximum: 255 },
                       format: { with: VALID_EMAIL_REGEX },
