5421d9e5fd28eff88ce18f165b06c2be5fb4df45
AnaTofuZ <anatofuz@gmail.com>
2017-10-19 21:50:01 +0900

    downしていたmigrateファイルを削除

--- db/migrate/20171019101953_add_token_user_uid_to_users.rb
+++
@@ -1,7 +0,0 @@
-class AddTokenUserUidToUsers < ActiveRecord::Migration[5.1]
-  def change
-    add_column :users, :provider, :string
-    add_column :users, :uid, :string
-    add_column :users, :token, :string
-  end
-end
