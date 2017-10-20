a7d63252a3c57a8885022f4638dd4a5cee3c5626
psato <forhighlow@gmail.com>
2017-10-20 17:08:10 +0900

    static index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
   get  '/help',  to:'static_pages#help'
+  get  '/list',  to:'static_pages#list'
   get  '/signup',  to:'users#new'
   resources :users
 end
