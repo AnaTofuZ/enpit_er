5892fdbdb10ddc0b633195dd949c4e8e4c18d54e
doubleQvel <rxn302n1@yahoo.co.jp>
2017-10-20 09:15:58 +0000

    Notify

--- app/controllers/line_notify_controller.rb
+++ app/controllers/line_notify_controller.rb
@@ -1,11 +1,14 @@
 class LineNotifyController < ApplicationController
   include LineNotifyHelper
+  protect_from_forgery :only => ["authorize"]
   def authorize
     #認証,トークン取得,通知はここから
-    if(params[:code].empty? && params[:state].empty?)
+    if !params[:code]
       line_notify_authorize
-    elsif(params[:state] == "aaa")
+    elsif params[:state] == "aaa"
       line_notify_get_token params[:code]
     end
   end
+  # get '/authorize', to:'line_notify#authorize'
+  # post '/authorize', to:'line_notify#authorize'
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-
   root 'static_pages#home'
   get  '/help',  to:'static_pages#help'
   get  '/signup',  to:'users#new'
   resources :users
-  get '/authorize', to:'line_notify#authorize'
-  post '/authorize', to:'line_notify#authorize'
 end
