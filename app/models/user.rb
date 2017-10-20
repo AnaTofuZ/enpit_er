f0e66f43fef171446a873d3a07ac481e8d1736cb
AnaTofuZ <anatofuz@gmail.com>
2017-10-20 11:40:50 +0900

    has_secure_passwordを削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,4 @@ class User < ApplicationRecord
     validates :email, presence: true, length: { maximum: 255 },
                       format: { with: VALID_EMAIL_REGEX },
                       uniqueness: { case_sensitive: false }
-    has_secure_password
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -7,13 +7,13 @@ class UserTest < ActiveSupport::TestCase
     password: "foobar", password_confirmation: "foobar")
   end
 
-#  test "should be valid" do
-#    assert @user.valid?
-#  end
-#
-#  test "name should be present" do
-#    @user.name = "     "
-#    assert_not @user.valid?
-#  end
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "name should be present" do
+    @user.name = "     "
+    assert_not @user.valid?
+  end
 
 end
