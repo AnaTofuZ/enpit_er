12cdfaf64d4397f9b19ebd002c3726d7b40be384
AnaTofuZ <anatofuz@gmail.com>
2017-10-23 14:41:05 +0900

    line通知用のrouteを変更

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get  '/help',  to:'static_pages#help'
   get  '/signup',  to:'users#new'
+  get '/authorize', to:'line_notify#authorize'
+  post '/authorize', to:'line_notify#authorize'
   resources :users
 end
