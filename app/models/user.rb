948585a177f3171a6d65bdba5bb733a85b6dcd91
AnaTofuZ <anatofuz@gmail.com>
2017-10-26 17:07:17 +0900

    meeting_placesのmodelとスキーマ定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    has_one :meeting_place
     validates :name,  presence: true, length: { maximum: 50 }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, length: { maximum: 255 },
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = User.new(name: "Example User", email: "user@example.com")
+  end
+
   test "should get new" do
     get signup_path
     assert_response :success
