054662026263fa48d27957b7960c443420f33ac0
AnaTofuZ <anatofuz@gmail.com>
2017-10-27 20:40:39 +0900

    気合でsessionController作った

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   root 'static_pages#home'
 
   get 'receipt/input'
@@ -7,8 +8,12 @@ Rails.application.routes.draw do
   get  '/friends',  to:'static_pages#friends'
   get  '/recipe',  to:'static_pages#recipe'
   get  '/signup',  to:'users#new'
+  get  '/login', to:'sessions#new'
+  post '/login', to:'sessions#create'
+  delete  '/logout', to:'sessions#destroy'
   get '/authorize', to:'line_notify#authorize'
   post '/authorize', to:'line_notify#authorize'
+
   resources :users
   resource :profile ,except: :destroy
 end
