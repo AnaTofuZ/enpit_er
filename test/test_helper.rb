6403a3d0476afe1217ac61360f85b7168fa67dcc
AnaTofuZ <anatofuz@gmail.com>
2017-10-27 21:30:52 +0900

    とりあえずsession出来た(テストしてない)

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,5 +7,8 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
-  # Add more helper methods to be used by all tests here...
+  def is_logged_id?
+    !session[:user_id].nil?
+  end
+
 end
