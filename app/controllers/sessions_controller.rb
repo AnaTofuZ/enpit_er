3c81c63c95412d6e3ef025fb633a955a575169ed
AnaTofuZ <anatofuz@gmail.com>
2017-10-27 21:01:09 +0900

    某チュートリアル風にsession

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,17 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
+      ## TODO:気合でリダイレクトj
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
 end
