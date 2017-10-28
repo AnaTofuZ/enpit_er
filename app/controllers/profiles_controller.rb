26b5aa23a66896a462e5d86bd6fbafee8be6840f
AnaTofuZ <anatofuz@gmail.com>
2017-10-28 15:00:52 +0900

    before_actionの使用に変更

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,19 +1,26 @@
 class ProfilesController < ApplicationController
+  before_action :logged_in_user,except: [:destroy]
   def new
     @user = user
-
-    if @user.nil?
-      flash[:notice] = "ログインしてくれ!"
-#      redirect_to action: 'index'
-      redirect_to controller: 'sessions', action: 'new'
-    end
-
   end
 
+def create
+end
 
   private
 
   def user
     @user ||= User.find_by(id: session[:user_id])
+  end
+
+  def prof_param
+     params.require(:profile).permit(:place)
+  end
+
+  def logged_in_user
+      unless logged_in?
+      flash[:notice] = "ログインしてくれ!"
+      redirect_to controller: 'sessions', action: 'new'
+      end
   end
 end
