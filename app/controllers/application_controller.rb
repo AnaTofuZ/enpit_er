5c9047c64903027aead1e0ceda9d6920091dd823
AnaTofuZ <anatofuz@gmail.com>
2017-10-28 16:41:43 +0900

    logged_inをapplicationに移動

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,13 @@ class ApplicationController < ActionController::Base
   def hello
     render html: "hello, world!and hogehoge"
   end
+
+  private
+
+  def logged_in_user
+      unless logged_in?
+      flash[:notice] = "ログインしてくれ!"
+      redirect_to login_url
+      end
+  end
 end
