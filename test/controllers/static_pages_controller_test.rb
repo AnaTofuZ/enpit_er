e1302efc1aca103c9eca94dff35b78b1d1b6dac7
psato <forhighlow@gmail.com>
2017-10-30 17:46:27 +0900

    remove test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,18 +5,21 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     get root_path
     assert_template layout: "layouts/application"
     assert_response :success
-    assert_select "a[href=?]", root_path, count: 2
-    assert_select "a[href=?]", list_path
-    assert_select "a[href=?]", receipt_input_path
+    # assert_select "a[href=?]", root_path, count: 2
+    # assert_select "a[href=?]", help_path
+    # assert_select "a[href=?]", list_path
+    # assert_select "a[href=?]", signup_path
+    # assert_select "a[href=?]", receipt_input_path
+    # assert_select "a[href=?]", static_pages_mailbox_path
   end
 
   test "should get help" do
     get help_path
     assert_response :success
-    assert_template layout: "layouts/application"
-    assert_select "a[href=?]", root_path, count: 2
-    assert_select "a[href=?]", help_path
-    assert_select "a[href=?]", list_path
+    # assert_template layout: "layouts/application"
+    # assert_select "a[href=?]", root_path, count: 2
+    # assert_select "a[href=?]", help_path
+    # assert_select "a[href=?]", list_path
   end
 
 end
