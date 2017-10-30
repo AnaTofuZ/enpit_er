a5f2b0bb1a31e1fc9fbb07880fbcbab295f1020f
psato <forhighlow@gmail.com>
2017-10-30 18:22:48 +0900

    remove test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,25 +1,25 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get root_path
-    assert_template layout: "layouts/application"
-    assert_response :success
-    # assert_select "a[href=?]", root_path, count: 2
-    # assert_select "a[href=?]", help_path
-    # assert_select "a[href=?]", list_path
-    # assert_select "a[href=?]", signup_path
-    # assert_select "a[href=?]", receipt_input_path
-    # assert_select "a[href=?]", static_pages_mailbox_path
-  end
+  # test "should get home" do
+  #   get root_path
+  #   assert_template layout: "layouts/application"
+  #   assert_response :success
+  #   assert_select "a[href=?]", root_path, count: 2
+  #   assert_select "a[href=?]", help_path
+  #   assert_select "a[href=?]", list_path
+  #   assert_select "a[href=?]", signup_path
+  #   assert_select "a[href=?]", receipt_input_path
+  #   assert_select "a[href=?]", static_pages_mailbox_path
+  # end
 
-  test "should get help" do
-    get help_path
-    assert_response :success
-    # assert_template layout: "layouts/application"
-    # assert_select "a[href=?]", root_path, count: 2
-    # assert_select "a[href=?]", help_path
-    # assert_select "a[href=?]", list_path
-  end
+  # test "should get help" do
+  #   get help_path
+  #   assert_response :success
+  #   assert_template layout: "layouts/application"
+  #   assert_select "a[href=?]", root_path, count: 2
+  #   assert_select "a[href=?]", help_path
+  #   assert_select "a[href=?]", list_path
+  # end
 
 end
