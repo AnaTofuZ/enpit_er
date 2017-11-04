940a03521a99c5c6b2603317053e3950e138a128
doubleQvel <rxn302n1@yahoo.co.jp>
2017-11-04 06:20:10 +0000

    ログイン関係ない

--- app/controllers/line_notify_controller.rb
+++ app/controllers/line_notify_controller.rb
@@ -4,9 +4,7 @@ class LineNotifyController < ApplicationController
   def authorize
     #認証,トークン取得が行える
     if !params[:code]
-      if logged_in?
         line_notify_authorize
-      end
     elsif params[:state] == "aa" #ハッシュ機能か何かでそいつ自身の暗号を作成しておく 
       token = line_notify_get_token params[:code]
       user=User.find(1)
