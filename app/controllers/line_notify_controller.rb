958b1a0ff27a621719d5b8b34812c7660611bc61
doubleQvel <rxn302n1@yahoo.co.jp>
2017-11-04 02:46:06 +0000

    ログインの確認

--- app/controllers/line_notify_controller.rb
+++ app/controllers/line_notify_controller.rb
@@ -2,11 +2,13 @@ class LineNotifyController < ApplicationController
   include LineNotifyHelper
   protect_from_forgery :only => ["authorize"]
   def authorize
-    #認証,トークン取得が行える
-    if !params[:code]
-      line_notify_authorize
-    elsif params[:state] == "aa" #ハッシュ機能か何かでそいつ自身の暗号を作成しておく 
-      line_notify_get_token params[:code]
+    if logged_in?
+      #認証,トークン取得が行える
+      if !params[:code]
+        line_notify_authorize
+      elsif params[:state] == "aa" #ハッシュ機能か何かでそいつ自身の暗号を作成しておく 
+        line_notify_get_token params[:code]
+      end
     end
   end
 end
