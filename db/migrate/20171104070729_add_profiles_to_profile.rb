13056d547b4ea51318d25b2b7625e51a4af92a9e
psato <forhighlow@gmail.com>
2017-11-04 19:45:18 +0900

    マイグレーションファイルが見づらかったので整形．

--- db/migrate/20171104070729_add_profiles_to_profile.rb
+++ db/migrate/20171104070729_add_profiles_to_profile.rb
@@ -1,10 +1,10 @@
 class AddProfilesToProfile < ActiveRecord::Migration[5.1]
   def change
     change_table :profiles do |t|
-      t.integer :sex ,default: 1,null: false, limit: 1
-      t.date :birthday,default: 1,null: false, index: true
-      t.string :job, index: true
-      t.string :hobby
+      t.integer  :sex ,    default: 1,null: false, limit: 1
+      t.date     :birthday,default: 1,null: false, index: true
+      t.string   :job,     index: true
+      t.string   :hobby
     end
   end
 end
