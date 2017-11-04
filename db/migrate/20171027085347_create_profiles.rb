a96c26280e0c241b548958ac0cd76ba0d631393e
psato <forhighlow@gmail.com>
2017-11-04 13:43:11 +0900

    生年月日を格納するdate型のカラムをprofileテーブルに追加．

--- db/migrate/20171027085347_create_profiles.rb
+++ db/migrate/20171027085347_create_profiles.rb
@@ -3,7 +3,7 @@ class CreateProfiles < ActiveRecord::Migration[5.1]
     create_table :profiles do |t|
       t.belongs_to :user, index:true
       t.integer :place_id
-
+      t.date :birthday
       t.timestamps
     end
   end
