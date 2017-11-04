4b89a4e8a68efd026eb5c94a23b2c8e6fc3e81fb
doubleQvel <rxn302n1@yahoo.co.jp>
2017-11-04 02:37:07 +0000

    dbにnotifytokenを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171027085347) do
+ActiveRecord::Schema.define(version: 20171104023423) do
+
   create_table "profiles", force: :cascade do |t|
     t.integer "user_id"
     t.integer "place_id"
@@ -25,6 +26,7 @@ ActiveRecord::Schema.define(version: 20171027085347) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "notifytoken"
   end
 
 end
