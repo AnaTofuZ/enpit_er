74c93f2d6cbe89d95286bf70a10f3308f8e6cd7b
doubleQvel <rxn302n1@yahoo.co.jp>
2017-11-06 08:40:37 +0000

    GETとPOSTの分割

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   post '/login', to:'sessions#create'
   delete  '/logout', to:'sessions#destroy'
   get '/authorize', to:'line_notify#authorize'
-  post '/authorize', to:'line_notify#authorize'
+  post '/authorize', to:'line_notify#token'
 
   # commityな部分
   get 'static_pages/community'
