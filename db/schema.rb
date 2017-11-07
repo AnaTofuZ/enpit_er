138c9ea8089c1fb7061652bcb82a627555a0e62d
AnaTofuZ <anatofuz@gmail.com>
2017-11-07 23:29:55 +0900

    defaultをdate型に修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171104070729) do
+ActiveRecord::Schema.define(version: 20171107142102) do
 
   create_table "profiles", force: :cascade do |t|
     t.integer "user_id"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 20171104070729) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "sex", limit: 1, default: 1, null: false
-    t.date "birthday", null: false
+    t.date "birthday", default: "1997-01-01", null: false
     t.string "job"
     t.string "hobby"
     t.string "purpose"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20171104070729) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "notifytoken"
   end
 
 end
