e3335e9733da95295e77d19aeae809dae9236e84
AnaTofuZ <anatofuz@gmail.com>
2017-11-07 23:43:42 +0900

    migrateファイルを削除してsed

--- db/migrate/20171107142102_change_datatype_profiles_of_birthday.rb
+++
@@ -1,9 +0,0 @@
-class ChangeDatatypeProfilesOfBirthday < ActiveRecord::Migration[5.1]
-  def up
-      change_column :profiles, :birthday, :date, null: false, default: '1997-01-1',index: true
-  end
-
-  def down
-      change_column :profiles, :birthday, :date, null: false, default: 1,index: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171107142102) do
+ActiveRecord::Schema.define(version: 20171104070729) do
 
   create_table "profiles", force: :cascade do |t|
     t.integer "user_id"
@@ -31,7 +31,6 @@ ActiveRecord::Schema.define(version: 20171107142102) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
-    t.string "notifytoken"
   end
 
 end
