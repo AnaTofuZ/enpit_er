167681928c030f9f6889a0eae6b11876435b70f4
doubleQvel <rxn302n1@yahoo.co.jp>
2017-11-09 14:08:45 +0000

    メッセージ送信ページの作成

--- app/controllers/line_notify_controller.rb
+++ app/controllers/line_notify_controller.rb
@@ -17,7 +17,14 @@ class LineNotifyController < ApplicationController
     end
   end
   def sending
-    #パラメータよりメッセージの作成
+    #パラメータよりメッセージの作成:param一覧[comunity,member,recipe,place,date]
+    users = User.find((params[:usersId].map(&:to_i)))
+    message = "レシコミからです!!"+"\nコミュニティ名:"+params[:comunity]+"\n集合場所:"+params[:placeName]+"\n集合日時:"+params[:date]
+              #+"\nレシピ"+ params[:recipe] 
+              #+"\nメンバー"+users.name
+    users.each do |m|  
+      line_notify_send_message(m.notifytoken,message)
+    end
     redirect_to root_url
   end
 end
