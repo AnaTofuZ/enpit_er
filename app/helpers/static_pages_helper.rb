f9b6a9d66beeb706526468171dbea366807ec6fb
AnaTofuZ <anatofuz@gmail.com>
2017-11-14 15:55:10 +0900

    loginの有無によってviewを修正

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,13 @@
 module StaticPagesHelper
+    def log_in(user)
+        session[:user_id] = user.id
+    end
+
+    def current_user
+        @current_user ||= User.find_by(id: session[:user_id])
+    end
+
+    def logged_in?
+        !current_user.nil?
+    end
 end
