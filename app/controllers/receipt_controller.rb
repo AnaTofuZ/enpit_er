e1bfaf89c87863b87989a98a9e5c2d9365c20203
doubleQvel <rxn302n1@yahoo.co.jp>
2017-11-18 06:55:05 +0000

    controllerの設定(写真の保存)

--- app/controllers/receipt_controller.rb
+++ app/controllers/receipt_controller.rb
@@ -2,5 +2,18 @@ class ReceiptController < ApplicationController
   def input
   end
   def confrimate
+    uploaded_file = fileupload_param[:file]
+    output_path = Rails.root.join('public', uploaded_file.original_filename)
+    
+    File.open(output_path, 'w+b') do |fp|
+      fp.write  uploaded_file.read
+    end
+    @img_path = "/"+uploaded_file.original_filename
+    @deleteFile = Rails.root.join('public', uploaded_file.original_filename)
   end
+  
+  private
+	def fileupload_param
+	  params.require(:fileupload).permit(:file)
+	end
 end
