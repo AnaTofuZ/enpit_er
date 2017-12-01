1a7dccc9a9eae429d210cd89e9faeb1897478123
doubleQvel <rxn302n1@yahoo.co.jp>
2017-12-01 08:54:38 +0000

    合計からしたの切り出し

--- app/controllers/receipt_controller.rb
+++ app/controllers/receipt_controller.rb
@@ -5,15 +5,31 @@ class ReceiptController < ApplicationController
   def input
   end
   def confrimate
-    uploaded_file = fileupload_param[:file]
-    output_path = Rails.root.join('public', uploaded_file.original_filename)
-    File.open(output_path, 'w+b') do |fp|
-      fp.write  uploaded_file.read
+    if params[:fileupload]
+      uploaded_file = fileupload_param[:file]
+      output_path = Rails.root.join('public', uploaded_file.original_filename)
+      File.open(output_path, 'w+b') do |fp|
+        fp.write  uploaded_file.read
+      end
+      @img_path = "/"+uploaded_file.original_filename
+      @deleteFile = Rails.root.join('public', uploaded_file.original_filename)
+      
+      result = ocr 
+      if result['error']
+        @message = result['error']['message']
+      elsif result['responses']
+        @text = result['responses'][0]['fullTextAnnotation']['text']
+      end
+      File.delete(@deleteFile)
     end
-    @img_path = "/"+uploaded_file.original_filename
-    @deleteFile = Rails.root.join('public', uploaded_file.original_filename)
-    
-    api_key = ENV["VISION_KEY"]
+  end
+  
+  private
+	def fileupload_param
+	  params.require(:fileupload).permit(:file)
+	end
+	def ocr
+	  api_key = ENV["VISION_KEY"]
     gurl = "https://vision.googleapis.com/v1/images:annotate?key=#{api_key}"
     get_url = URI.parse(gurl)
     http = Net::HTTP.new(get_url.host, get_url.port)
@@ -25,15 +41,6 @@ class ReceiptController < ApplicationController
     res = http.request(req,body)
     result = JSON.parse(res.body)
     logger.debug(result)
-    if result['error']
-      @message = result['error']['message']
-    elsif result['responses']
-      @text = result['responses'][0]['fullTextAnnotation']['text']
-    end
-  end
-  
-  private
-	def fileupload_param
-	  params.require(:fileupload).permit(:file)
+	  return result
 	end
 end
