978c54daa38037f483d6ea1a62e3cfc988f63da2
AnaTofuZ <anatofuz@gmail.com>
2017-12-04 15:47:49 +0900

    複数検索のためwhereに修正

--- app/controllers/line_notify_controller.rb
+++ app/controllers/line_notify_controller.rb
@@ -23,7 +23,7 @@ class LineNotifyController < ApplicationController
 
     access_counter #アクセスカウンタ
     #パラメータよりメッセージの作成:param一覧[comunity,member,recipe,place,date]
-    profiles = Profile.find((params[:usersId].map(&:to_i)))
+    profiles = Profile.where(user_id: (params[:usersId].map(&:to_i)))
     message = "レシコミからです!!"+"\nコミュニティ名:"+params[:comunity]+"\n集合場所:"+params[:placeName]+"\n集合日時:"+params[:date]
     message << "メンバー\n"
 
