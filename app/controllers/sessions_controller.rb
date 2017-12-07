b24289d38d1a61c8402f5359c26efb98f108d2c9
nemunemu <e155719@ie.u-ryukyu.ac.jp>
2017-12-07 16:26:07 +0900

    日本語に修正

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
       log_in user
       redirect_to user
     else
-      flash.now[:danger] = 'Invalid email/password combination'
+      flash.now[:danger] = 'メールアドレスか, パスワードが間違っています'
       render 'new'
     end
   end
