f45505caa8334ebabb8e4b6023c102c5c043a90f
psato <forhighlow@gmail.com>
2017-12-07 18:32:32 +0900

    emailの要素を削除して，migrate.emailを使わず，名前だけで特定．

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,5 @@ class User < ApplicationRecord
     has_one :item
     validates :name,  presence: true, length: { maximum: 50 }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-    validates :email, presence: true, length: { maximum: 255 },
-                      format: { with: VALID_EMAIL_REGEX },
-                      uniqueness: { case_sensitive: false }
     has_secure_password
 end
