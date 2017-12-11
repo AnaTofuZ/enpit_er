d4fbc4d63aaf0ea5e8de14b5ec67416dfb0e7bc1
AnaTofuZ <anatofuz@gmail.com>
2017-12-11 14:00:31 +0900

    バリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,13 @@ class User < ApplicationRecord
     has_many :item
     validates :name,  presence: true, length: { maximum: 50 }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    VALID_NAME_REGEX = /\A\w+\z/i
     validates :email, presence: true, length: { maximum: 255 },
                       format: { with: VALID_EMAIL_REGEX },
                       uniqueness: { case_sensitive: false }
+    validates  :screen_name, presence: true, length: { maximum: 15},
+                     format: { with: VALID_NAME_REGEX},
+                      uniqueness: { case_sensitive: false }
+
     has_secure_password
 end
