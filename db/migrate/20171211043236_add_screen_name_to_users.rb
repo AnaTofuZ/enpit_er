f6b3debbf74ba0757a31ca311cff523c80b7b301
AnaTofuZ <anatofuz@gmail.com>
2017-12-11 13:49:23 +0900

    rails db:migrate

--- db/migrate/20171211043236_add_screen_name_to_users.rb
+++ db/migrate/20171211043236_add_screen_name_to_users.rb
@@ -2,5 +2,6 @@ class AddScreenNameToUsers < ActiveRecord::Migration[5.1]
   def change
     add_column :users, :screen_name, :string
     add_index :users, :screen_name, unique: true
+    
   end
 end
